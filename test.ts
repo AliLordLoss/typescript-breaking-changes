e214fd3d62cccbdf940a0149f660407988fe320e
AliLordLoss <a.a.ghanati@gmail.com>
2026-07-28 16:25:02 +0200

    fix: removed redundant file

--- test.ts
+++
@@ -1,7 +0,0 @@
-class Base<T extends NonNullable<Partial<Readonly<{ id: string }>>>> {}
-
-class C<
-  T extends NonNullable<Partial<Readonly<{ id: string }>>>,
-> extends Base<T> {}
-
-const c = new C();
